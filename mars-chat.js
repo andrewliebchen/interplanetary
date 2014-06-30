ed12dd800dd5d54454e2f872dee5fabdaed8cf2f
andrewliebchen <andrewliebchen@gmail.com>
2014-06-29 19:50:35 -0700

    Inital set up and some basics

---
+++ mars-chat.js
@@ -0,0 +1,38 @@
+Messages = new Meteor.Collection('messages');
+
+if (Meteor.isClient) {
+  Session.setDefault('currentUserName', 'Anonymous');
+  Session.setDefault('currentUserLocation', 'Earth');
+
+  Template.messages.messages = function() {
+    return Messages.find({}, {sort: {time: -1}});
+  }
+
+  Template.newMessage.events({
+    'keydown #new_message' : function(event) {
+      if(event.which == 13) {
+        var message = $(event.target).val();
+
+        if(message) {
+          Message.insert({
+            message:  message,
+            name:     Session.get('currentUserName'),
+            date:     Date.now(),
+            location: Session.get('currentUserLocation')
+          });
+        }
+      }
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // Add some dummy info
+    if(Messages.find().count() === 0) {
+      Messages.insert({message: "Greetings from Earth!", name: "Jeff", date: Date.now(), location: "Earth"});
+      Messages.insert({message: "Mars is so cold!", name: "Jon", date: Date.now(), location: "Mars"});
+      Messages.insert({message: "We'll send up some warmer jackets, FWIW.", name: "Jennifer", date: Date.now(), location: "Earth"});
+    }
+  });
+}
