90355809567e1f8220d29d46e2b186ad3eb19738
andrewliebchen <andrewliebchen@gmail.com>
2014-06-29 21:13:26 -0700

    Calculate posted and view time

--- mars-chat.js
+++ mars-chat.js
@@ -4,26 +4,35 @@ if (Meteor.isClient) {
 
   Session.setDefault('currentUserName', 'Anonymous');
   Session.setDefault('currentUserLocation', 'Earth');
+  Session.setDefault('currentTime', Date.now());
+
+  Meteor.startup(function () {
+    setInterval(function () {
+      Session.set('currentTime', Date.now());
+    }, 1000);
+  });
 
   Template.messages.messages = function() {
     return Messages.find({}, {sort: {time: -1}});
   };
 
-  Template.messages.distance = function() {
-    return distance();
+  Template.messages.latency = function() {
+    return earthMarsDistance();
   };
 
   Template.newMessage.events({
     'keydown #new_message' : function(event) {
       if(event.which == 13) {
         var message = $(event.target).val();
+        var viewDate = Date.now() + (earthMarsDistance() * 1000);
 
         if(message) {
-          Message.insert({
-            message:  message,
-            name:     Session.get('currentUserName'),
-            date:     Date.now(),
-            location: Session.get('currentUserLocation')
+          Messages.insert({
+            message:    message,
+            name:       Session.get('currentUserName'),
+            postedDate: Date.now(),
+            viewDate:   viewDate,
+            location:   Session.get('currentUserLocation')
           });
         }
       }
@@ -33,11 +42,14 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    // Delete database
+    // Messages.remove({});
+
     // Add some dummy info
     if(Messages.find().count() === 0) {
-      Messages.insert({message: "Greetings from Earth!", name: "Jeff", date: Date.now(), location: "Earth"});
-      Messages.insert({message: "Mars is so cold!", name: "Jon", date: Date.now(), location: "Mars"});
-      Messages.insert({message: "We'll send up some warmer jackets, FWIW.", name: "Jennifer", date: Date.now(), location: "Earth"});
+      Messages.insert({message: "Greetings from Earth!", name: "Jeff", postedDate: Date.now(), location: "Earth"});
+      Messages.insert({message: "Mars is so cold!", name: "Jon", postedDate: Date.now(), location: "Mars"});
+      Messages.insert({message: "We'll send up some warmer jackets, FWIW.", name: "Jennifer", postedDate: Date.now(), location: "Earth"});
     }
   });
 }
