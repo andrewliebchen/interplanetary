251449daedc7ba501029b47056b00790dbe1f9b2
andrewliebchen <andrewliebchen@gmail.com>
2014-07-14 23:50:24 -0700

    Trying to make the stupd message passing slider work

--- interplanetary.js
+++ interplanetary.js
@@ -1,69 +1,37 @@
-Messages = new Meteor.Collection('messages');
+// Messages = new Meteor.Collection('messages');
 
 if (Meteor.isClient) {
 
-  Session.setDefault('currentUserName', 'Anonymous');
-  Session.setDefault('currentUserLocation', 'Earth');
   Session.setDefault('currentTime', Date.now());
-  Session.setDefault('countdown', 10000);
 
   Meteor.startup(function() {
-    // Check the time every second
+    // Do something every second
     setInterval(function() {
       Session.set('currentTime', Date.now());
+      // var travelDistance = 1- (Session.get('currentTime') - Session.get('triggerTime')) / (Session.get('triggerTime') - Session.get('arrivalTime'));
+      // $('.marker').css('left', travelDistance + '%');
     }, 1000);
   });
 
-  Template.messages.messages = function() {
-    var viewDateTarget = Session.get('currentTime') - earthMarsLatency();
-    return Messages.find({viewDate: {$lt: viewDateTarget}}, {sort: {time: -1}});
+  UI.body.latency = function() {
+    return earthMarsLatency('m');
   };
 
-  Template.messages.latency = function() {
-    return earthMarsLatency();
-  };
+  Template.distance.events({
+    'click .distance' : function(event, template) {
+      // var currentTime = Session.get('currentTime');
+      // Session.setDefault('triggerTime', currentTime);
+      // Session.setDefault('currentLatency', earthMarsLatency());
+      // Session.setDefault('arrivalTime', currentTime + earthMarsLatency());
 
-  Template.signIn.events({
-    'click #sign_in_submit' : function(event) {
-      var $this = $(event.target);
-      Session.set('currentUserName', $('#first_name').val());
-      Session.set('currentUserLocation', $('#location input:checked').val());
-    }
-  });
-
-  Template.newMessage.events({
-    'keydown #new_message' : function(event) {
-      if(event.which == 13) {
-        var $this = $(event.target);
-        var message = $this.val();
-        var viewDate = Date.now() + (earthMarsLatency() * 60000);
-
-        if(message) {
-          Messages.insert({
-            message:    message,
-            name:       Session.get('currentUserName'),
-            postedDate: Date.now(),
-            viewDate:   viewDate,
-            location:   Session.get('currentUserLocation')
-          });
-        };
-
-        $this.val('');
-      }
+      var latency = earthMarsLatency('s');
+      // $('.marker').css('animation-duration', latency + 's');
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // Delete database
-    // Messages.remove({});
 
-    // Add some dummy info
-    if(Messages.find().count() === 0) {
-      Messages.insert({message: "Greetings from Earth!", name: "Jeff", postedDate: Date.now(), viewDate: Date.now(), location: "Earth"});
-      Messages.insert({message: "Mars is so cold!", name: "Jon", postedDate: Date.now(), viewDate: Date.now(), location: "Mars"});
-      Messages.insert({message: "We'll send up some warmer jackets, FWIW.", name: "Jennifer", postedDate: Date.now(), viewDate: Date.now(), location: "Earth"});
-    }
   });
 }
