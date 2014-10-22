98de6f0abd7903a3365ebfee4f57bcdfa6b0e7af
andrewliebchen <andrewliebchen@gmail.com>
2014-10-21 23:13:56 -0700

    Update meteor, wire up message

--- interplanetary.js
+++ interplanetary.js
@@ -1,37 +1,18 @@
-// Messages = new Meteor.Collection('messages');
-
 if (Meteor.isClient) {
 
   Session.setDefault('currentTime', Date.now());
 
-  Meteor.startup(function() {
-    // Do something every second
-    setInterval(function() {
-      Session.set('currentTime', Date.now());
-      // var travelDistance = 1- (Session.get('currentTime') - Session.get('triggerTime')) / (Session.get('triggerTime') - Session.get('arrivalTime'));
-      // $('.marker').css('left', travelDistance + '%');
-    }, 1000);
-  });
+  UI.body.helpers({
+    'latency': function() {
+      return moment().add(earthMarsLatency(), 'minutes').calendar();
+    },
 
-  UI.body.latency = function() {
-    return earthMarsLatency('m');
-  };
-
-  Template.distance.events({
-    'click .distance' : function(event, template) {
-      // var currentTime = Session.get('currentTime');
-      // Session.setDefault('triggerTime', currentTime);
-      // Session.setDefault('currentLatency', earthMarsLatency());
-      // Session.setDefault('arrivalTime', currentTime + earthMarsLatency());
-
-      var latency = earthMarsLatency('s');
-      // $('.marker').css('animation-duration', latency + 's');
+    'travelTime': function() {
+      return moment(earthMarsLatency(), 'minutes').format('m:ss');
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
 
-  });
 }
