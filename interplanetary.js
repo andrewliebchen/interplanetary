25b1f53bdd02f05e4e37e0f7a33e0279cb893d90
andrewliebchen <andrewliebchen@gmail.com>
2014-10-21 23:48:04 -0700

    WIP

--- interplanetary.js
+++ interplanetary.js
@@ -1,7 +1,6 @@
+Messages = new Meteor.Collection('messages');
+
 if (Meteor.isClient) {
-
-  Session.setDefault('currentTime', Date.now());
-
   UI.body.helpers({
     'latency': function() {
       return moment().add(earthMarsLatency(), 'minutes').calendar();
@@ -9,6 +8,28 @@ if (Meteor.isClient) {
 
     'travelTime': function() {
       return moment(earthMarsLatency(), 'minutes').format('m:ss');
+    }
+  });
+
+  Template.message.helpers({
+    'status': function() {
+      var currentMessage = Messages.find({}).fetch();
+      console.log(currentMessage.slice(1));
+      if(moment() == currentMessage.createdAt) {
+        return "Arrived!";
+      } else {
+        return "In transit";
+      }
+    }
+  });
+
+  Template.message.events({
+    'click .send-message': function(){
+      var now = moment();
+      Messages.insert({
+        message: "Hello world!",
+        timestamp: now
+      })
     }
   });
 }
