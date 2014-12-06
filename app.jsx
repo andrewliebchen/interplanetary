9cb10c57c6a06b9d9577a0f31a7d4a258b919cc2
andrewliebchen <andrewliebchen@gmail.com>
2014-12-06 13:53:22 -0800

    Get latency message to work

--- app.jsx
+++ app.jsx
@@ -6,26 +6,30 @@ var testLatency = 5000;
 
 Messages = new Meteor.Collection('messages');
 
-// if (Meteor.isClient) {
-//   UI.body.helpers({
-//     'latency': function() {
-//       return moment().add(earthMarsLatency(), 'minutes').calendar();
-//     },
-
-//     'travelTime': function() {
-//       return moment(earthMarsLatency(), 'minutes').format('m:ss');
-//     }
-//   });
-// }
-
 var App = React.createClass({
   mixins: [Meteor.Mixin],
 
-  getMeteorState: function() {},
+  getInitialState: function() {
+    return {
+      travelTime:  moment(earthMarsLatency(), 'minutes').format('m:ss'),
+      arrivalTime: moment().add(earthMarsLatency(), 'minutes').calendar()
+    };
+  },
 
   render: function() {
     return (
-      <div>Hello, world</div>
+      <div className="wrapper">
+        <div className="headline">
+          <h1>Tweet from Mars</h1>
+          <h2>Currently it takes {this.state.travelTime} for a message to travel between Mars and the Earth.</h2>
+          <h2>If you send your tweet now, it will be posted on Twitter {this.state.arrivalTime}</h2>
+          <button className="call-to-action">Log in with Twitter</button>
+        </div>
+        <div className="mars" />
+        <footer className="footer">
+          <a>Huh?</a>
+        </footer>
+      </div>
     );
   }
 });
